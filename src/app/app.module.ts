e345db0a582a6a254afcfe6d2d7f5784b3030bdf
David Sackstein <davids@codeprecise.com>
2021-07-30 22:23:57 +0300

    Layout in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module'
+import { MaterialModule } from './material.module';
+import { ProgressComponent } from './components/progress/progress.component';
+import { QuestionComponent } from './components/question/question.component';
+import { EndComponent } from './components/end/end.component';
+import { SummaryComponent } from './components/summary/summary.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressComponent,
+    QuestionComponent,
+    EndComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
