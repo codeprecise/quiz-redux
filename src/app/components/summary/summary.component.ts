6f0beabee74ba6a9fe1c648cc4434e96aa7b3ec0
David Sackstein <davids@codeprecise.com>
2021-07-31 22:43:12 +0300

    Summary

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { QUESTIONS } from 'src/app/models/data';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.scss']
+  styleUrls: ['./summary.component.scss'],
 })
 export class SummaryComponent implements OnInit {
+  answeredQuestions$ = of(
+    QUESTIONS.map((q) => ({ ...q, userAnswerIndex: 1 }))
+  );
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 const modules = [
   MatButtonModule,
   MatToolbarModule,
-  MatIconModule
+  MatIconModule,
+  MatCardModule
 ]
 
 @NgModule({
